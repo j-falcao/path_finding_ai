0051a4f2e218d55a4c747d1cd4264af2812a4c0f
Joao Falcao <joaorpalharesfalcao@gmail.com>
2026-03-23 15:36:20 +0100

    setting up fastapi app. cors. default graph and search requests

--- main.py
+++ main.py
@@ -1,49 +1,78 @@
-import json
-from algorithms import astar_search, depth_limited_search, uniform_cost_search, greedy_best_first_search
+from networkx import nx
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from algorithms import astar_search, depth_limited_search, greedy_best_first_search, uniform_cost_search
 from map import Map
 
-with open("data/map.json") as f:
-    data = json.load(f)
+ALGORITHMS = {
+    "ucs": uniform_cost_search,
+    "dls": depth_limited_search,
+    "greedy": greedy_best_first_search,
+    "astar": astar_search
+}
 
-m = Map()
+app = FastAPI()
 
-for city, neighbors in data.items():
-    for neighbor, dist in neighbors.items():
-        m.add_edge(city, neighbor, dist)
-""" 
-for city, neighbors in m.edges.items():
-    print(f"{city}: {neighbors}")
-""" 
+# Allowed origins
+origins = [
+    "http://localhost:5173", # Vue frontend
+]
 
-# TESTES
-
-# uniform_cost_search
-""" 
-cost, path = uniform_cost_search(m, "Aveiro", "Faro") 
-
-print(f"Cost: {cost}")
-print(f"Path: {path}") 
-"""
-
-# depth_limited_search
-""" 
-status, cost, path = depth_limited_search(m, "Lisboa", "Porto", limit=3)
-
-print(f"Status : {status}")
-print(f"Cost   : {cost} km")
-print(f"Path   : {' → '.join(path)}")
-""" 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
+@app.get("/api/graph")
+def default_graph():
+    G = nx.read_gml('data/map.gml')
 
-# HEURISTIC
-""" 
-with open("data/heuristic.json") as f:
-    data = json.load(f)
+    # Nodes
+    nodes = [{'data': {'id': str(n), 'label': str(n)}} for n in G.nodes()]
 
-cost, path = greedy_best_first_search(m, "Aveiro", "Faro", data)
+    # Edges with weight
+    edges = []
+    for u, v, attr in G.edges(data=True):
+        edges.append({
+            'data': {
+                'id': f"{u}-{v}",
+                'source': str(u),
+                'target': str(v),
+                'weight': attr.get('weight', 1)  # default to 1 if missing
+            }
+        })
 
-print(f"Cost: {cost}")
-print(f"Path: {path}")
-""" 
+    cy_data = {'nodes': nodes, 'edges': edges}
+    return cy_data
 
+
+
+class SearchRequest(BaseModel):
+    gml_data: str
+    start: str
+    goal: str
+    algorithm: str
+    depth: int = 10
+
+
+@app.post("/api/search")
+def search(req: SearchRequest):
+    m = Map.from_gml(req.gml_data)
+
+    algorithm = ALGORITHMS.get(req.algorithm)
+
+    if not algorithm:
+        raise HTTPException(400, "Unknown algorithm")
+
+    return algorithm(
+        m,
+        req.start,
+        req.goal,
+        heuristic=req.heuristic,
+        depth=req.depth
+    )
